c91612a7d1920ff73fa66d9905735c33bf377ae5
dvega23 <87511013+dvega23@users.noreply.github.com>
2023-01-28 10:57:29 -0500

    Add files via upload

---
+++ src/KUKA_3/kr3_master/src/traj_cart.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+
+import sys
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+group = moveit_commander.MoveGroupCommander("kuka_arm")
+display_trajectory_publisher = rospy.Publisher(
+    '/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=1)
+
+pose_target = geometry_msgs.msg.Pose()
+pose_target.orientation.w = 1.0
+#pose_target.orientation.x = 0.5
+#pose_target.orientation.y = 0.5
+#pose_target.orientation.z = 0.5
+
+pose_target.position.x = 0.5
+pose_target.position.y = -0.07
+pose_target.position.z = 0.722
+group.set_pose_target(pose_target)
+
+##plan1 = group.plan()
+
+plan = group.go(wait=True)
+# Calling `stop()` ensures that there is no residual movement
+group.stop()
+# It is always good to clear your targets after planning with poses.
+# Note: there is no equivalent function for clear_joint_value_targets()
+group.clear_pose_targets()
+rospy.sleep(5)
+
+moveit_commander.roscpp_shutdown()
